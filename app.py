0ccf077acab506e381c007ccf048c375b152109e
Carla Buongiorno <carlab201103@gmail.com>
2022-05-20 20:38:44 +0000

    Add Login functionality

--- app.py
+++ app.py
@@ -29,6 +29,9 @@ def home():
     return render_template("home.html")
 
 
+
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """
@@ -59,6 +62,39 @@ def register():
         flash("Registration Successful!", "success")
 
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """
+        Find username in db, check that user's password matches what's in
+        the db, render login page if no match, render profile page if match.
+    """
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")), "success")
+                return redirect(url_for(
+                    "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password", "danger")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password", "danger")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 @app.route("/get_events")
