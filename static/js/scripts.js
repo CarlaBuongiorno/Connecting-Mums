4cb61b139a5a9ff72472543808cd95ec6362bc0d
Florence Mezino <florence.mezino@outlook.com>
2022-05-21 07:46:54 +0000

    Added quoute generator, fixing header image, added suggestion display for events

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,51 @@
-/*!
-* Start Bootstrap - Business Frontpage v5.0.8 (https://startbootstrap.com/template/business-frontpage)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Get daily quotes to share
+const quoteGenerator = document.querySelector("#quotegenerator");
+quoteGenerator.addEventListener("click", generateQuote);
+
+const shareTweet = document.querySelector("#sharetweet");
+shareTweet.addEventListener("click", tweetThis);
+
+  const quotes = [
+    {
+    "quote": "Live in the moment and make it so beautiful that it will be worth remembering.",
+    "author": "Fanny Crosby"
+  },
+  {
+    "quote": "Sometimes the strength of motherhood is greater than natural laws.",
+    "author": "Barbara Kingsolver"
+  },
+  {
+    "quote": "Motherhood is the biggest gamble in the world. It is the glorious life force. It’s huge and scary – it’s an act of infinite optimism",
+    "author": "Gilda Radner"
+  },
+  {
+    "quote": "The days are long but the years are short.",
+    "author": "Gretchen Rubin"
+  },
+  {
+    "quote": "A mother continues to labor long after the baby is born.",
+    "author": "Lisa Jo Baker"
+  }
+];
+
+
+function generateQuote() {
+    const randomNumber = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[randomNumber];
+   document.getElementById("quote").textContent = quote.quote;
+   document.getElementById("author").textContent = quote.author;
+}
+
+generateQuote();
+
+function tweetThis() {
+    const url = "https://twitter.com/intent/tweet";
+    const text = document.getElementById('quote').textContent;
+    const author = document.getElementById('author').textContent;
+    window.open(url+"?text=" + "\"" + text + "\"" + " " + author);
+}
+
+tweetThis();
+
+
+
