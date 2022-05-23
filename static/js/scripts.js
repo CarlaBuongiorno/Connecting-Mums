7176ea0e36a4776786a26d08d02c0414efbafbd1
Florence Mezino <florence.mezino@outlook.com>
2022-05-23 10:07:48 +0000

    Added scroll bar to journal entries

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -71,3 +71,12 @@ function tweetThis() {
 
 tweetThis();
 
+// Scroll bar for journal
+var myCustomScrollbar = document.querySelector('.my-custom-scrollbar');
+var ps = new PerfectScrollbar(myCustomScrollbar);
+
+var scrollbarY = myCustomScrollbar.querySelector('.ps__rail-y');
+
+myCustomScrollbar.onscroll = function () {
+  scrollbarY.style.cssText = `top: ${this.scrollTop}px!important; height: 400px; right: ${-this.scrollLeft}px`;
+}
